384af2a8d5b478a89c942098d5cdbfa6f034770d
Dawid Iwaniuk <dawuso@gmail.com>
2021-04-19 17:13:12 +0200

    Add project files.

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Maker { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+
+    }
+}
